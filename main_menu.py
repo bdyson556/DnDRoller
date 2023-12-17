b1fad5b9d68344f6dbe438f6f475e425b1f62c65
bdyson556 <71849300+bdyson556@users.noreply.github.com>
2023-12-16 20:40:26 -0600

    Combined attack and damage rolls into one menu. Critical logic now working!

--- main_menu.py
+++ main_menu.py
@@ -25,26 +25,27 @@ class Menu:
             command=self.run_skill_check
         )
 
-        self.skill_check_button.pack()
-
         self.roll_initiative_button = tk.Button(self.window, text="Roll Initiative", command=self.run_initiative)
-        self.roll_initiative_button.pack()
 
         self.roll_attack_button = tk.Button(self.window, text="Attack", command=self.run_attack)
-        self.roll_attack_button.pack()
 
         # self.roll_for_damage_button = tk.Button(self.window, text="Roll for Damage", command=self.run_roll_for_damage)
         # self.roll_for_damage_button.pack()
 
         self.update_character_stats_button = tk.Button(self.window, text="Character Stats...",
                                                        command=self.character_stats_menu)
-        self.update_character_stats_button.pack()
 
         self.current_roll_result = None
 
         self.output_box = tk.Text(self.window, width=40, height=15)
-        self.output_box.pack()
         # self.output_box.grid(row=1, sticky="e")
+
+        self.skill_check_button.pack()
+        self.roll_initiative_button.pack()
+        self.roll_attack_button.pack()
+        self.update_character_stats_button.pack()
+        self.output_box.pack()
+
 
     # EXAMPLES OF "GRID()"
     # skill_box_label.grid(row=0, column=0, padx=10, pady=15, sticky="w")
@@ -62,23 +63,11 @@ class Menu:
         selected_skill = skill_var.get()
         print(selected_skill)
 
-    # def sneak_eligibility_menu(self, weapon):
-
     def character_stats_menu(self):
         messagebox.showinfo("Update Character Stats button clicked!")
 
     def show_roll_history(self):
         print("Roll history button clicked")
-        # roll_initiative_menu = tk.Toplevel(self.window)
-        # roll_initiative_menu.title("Skill Check")
-        # roll_initiative_menu.geometry("550x300")
-        #
-        # roll_button = tk.Button(
-        #     roll_initiative_menu,
-        #     text="Roll!",
-        #     command=lambda: self.display_roll_result(roll_initiative_menu, lambda: roll_initiative(advantage=False))
-        # )
-        # roll_button.pack(pady=20)
 
     def combined_functions(self, func_list):
         for f in func_list: f()
